9767b8757015fd8de423738acbe59113a280e77a
Antonio Testaì <atestai@gmail.com>
2021-07-17 14:27:10 +0200

    fine secondo endpoint - statistiche

--- backend/controllers/records.js
+++ backend/controllers/records.js
@@ -1,4 +1,5 @@
 
+const sequelize = require('sequelize');
 const RecordsModel = require('../models').Records;
 const {Workclasses, EducationLevels,  MaritalStatuses, Occupations, Relationships, Races, Sexes, Countries } = require("../models");
 
@@ -88,6 +89,65 @@ const Records = {
                 limit
             },
             data
+        });
+    },
+
+
+    async getStats(req, res){
+
+        console.log(req.params);
+        const {aggregationType=undefined} = req.params;
+        const {aggregationValue:aggregationFilter=undefined} = req.params;
+
+        if (!["age", "education_level_id", "occupation_id"].includes(aggregationType)) {
+            return res.status(422).json({
+                code : 1,
+                message : 'aggregationType missing or invalid'
+            });    
+        }
+
+        if (aggregationFilter === undefined) {
+            return res.status(422).json({
+                code : 2,
+                message : 'aggregationValue missing or invalid'
+            });    
+        }
+
+        
+        const where = {};
+        where[aggregationType] = aggregationFilter;
+
+        const data = await RecordsModel.findOne({
+            attributes :[ 
+                [sequelize.fn('sum', sequelize.col('capital_gain')), 'capital_gain_sum'],
+                [sequelize.fn('avg', sequelize.col('capital_gain')), 'capital_gain_avg'],
+                [sequelize.fn('sum', sequelize.col('capital_loss')), 'capital_loss_sum'],
+                [sequelize.fn('avg', sequelize.col('capital_loss')), 'capital_loss_avg'],
+            ],
+            where
+        });
+
+        //console.log(data.dataValues);
+
+        where.over_50k = true;
+        const over_50k_count = await RecordsModel.count({
+            where
+        });    
+
+        //console.log(data_over50k);
+
+        where.over_50k = false;
+        const under_50k_count = await RecordsModel.count({
+            where
+        });    
+
+
+        res.status(200).json({
+            aggregationType,
+            aggregationFilter : parseInt(aggregationFilter),
+            ...data.dataValues,
+            over_50k_count,
+            under_50k_count
         });
     }
 }
--- backend/routers/records.js
+++ backend/routers/records.js
@@ -5,7 +5,9 @@ const recordsControl = require('../controllers/records.js')
 const router = express.Router();
 
 router.get('/', recordsControl.getAll);
-router.get('/:bla', recordsControl.getAll);
+router.get('/stats/:aggregationType/:aggregationValue', recordsControl.getStats);
+
+
 
 
 module.exports = router;
